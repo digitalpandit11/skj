6243914b14d60a28a9d6f1cd454d7ecc39b1d45b
shitalw20 <133356300+shitalw20@users.noreply.github.com>
2023-06-20 13:58:47 +0530

    deleted admin folder

--- shri_krishna_jewellers_admin/application/views/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2020-2021 <a target="_blank" href="https://www.vbdigitech.com/">VB Digitech</a>.</strong>
-    All rights reserved.
-
-
-    
-  </footer>
-
-  
