8693adaff450b2aecb3901f00508ba4b380a2da3
Prakash Raje <prakash.solace123@gmail.com>
2025-04-23 18:07:39 +0530

    contact details changes

--- gold_jewellery/goldcastchainpendant.php
+++ gold_jewellery/goldcastchainpendant.php
@@ -457,100 +457,120 @@ position:absolute;
       </div>
     </section><!-- End Portfolio Section -->
 
-    <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact section-bg">
-      <div class="container">
+      <!-- ======= Contact Section ======= -->
+      <section id="contact" class="contact section-bg">
+          <div class="container">
 
-        <div class="section-title">
-          <h2 data-aos="fade-in">Contact</h2>
-          <p data-aos="fade-in"></p>
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-6">
-
-            <div class="row">
-              <div class="col-md-12">
-                <div class="info-box" data-aos="fade-up">
-                  <i class="bx bx-map"></i>
-                  <h3>Our Address</h3>
-                  <p>Amrut Society, Gujarat Colony, Kothrud, Pune-38</p>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
-                  <i class="bx bx">&#9742;</i>
-                  <h3>Telephone No.</h3>
-                  <p>020 2538 2115</p>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
-                  <i class="bx bx-phone-call"></i>
-                  <h3>Whatsapp / Mob No.</h3>
-                  <p>+91 7030635555</p>
-                </div>
-              </div>
-
-              <div class="col-md-12">
-                <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
-                  <h3>Follow Us</h3>
-                  <div class="social-links">
-                    <a href="https://www.facebook.com/ShriKrishnaJewellersKothrud" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
-                    <a href="https://instagram.com/shri.krishna.jewellers" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
-                    <a href="https://g.page/r/CZ6RgBm0-OdQEAg/review" class="google-plus" target="_blank"><i class="bx bxl-google"></i></a>
-                  </div>
-                  <br><br>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-          <div class="col-lg-6">
-            
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-up">
               <div class="section-title">
-                <h2 data-aos="fade-in">Get In Touch With Us</h2>
-            </div>
-              <div class="form-row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validate"></div>
-                </div>
-                <div class="col-md-6 form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validate"></div>
-                </div>
+                  <h2 data-aos="fade-in">Contact</h2>
+                  <p data-aos="fade-in"></p>
               </div>
 
-              <div class="form-group">
-                <input type="text" class="form-control" name="phone" id="phone" placeholder="Phone" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validate"></div>
+              <div class="row">
+
+                  <div class="col-lg-6">
+
+                      <div class="row">
+                      <div class="col-md-12">
+                              <div class="info-box" data-aos="fade-up">
+                                  <i class="bx bx-map"></i>
+                                  <h3>Our Address</h3>
+                                  <p>Amrut Society, Gujarat Colony, Kothrud, Pune-38</p>
+                              </div>
+                          </div>
+                          <div class="col-md-6">
+                              <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
+                                  <i class="bx bx">&#9742;</i>
+                                  <h3>Telephone No.</h3>
+                                  <p><a href="tel:02025382115">020 2538 2115</a></p>
+                              </div>
+                          </div>
+                          <div class="col-md-6">
+                              <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
+                                  <i class="bx bx-phone-call"></i>
+                                  <h3>Whatsapp / Mob No.</h3>
+                                  <p><a target="_blank" href="https://wa.me/7030635555">+91 7030635555</a> | <a
+                                          target="_blank" href="https://wa.me/9021909385">+91 9021909385</a></p>
+                              </div>
+                          </div>
+
+                          <div class="col-md-6">
+                              <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
+                                  <h3>Follow Us</h3>
+                                  <div class="social-links">
+                                      <a href="https://www.facebook.com/ShriKrishnaJewellersKothrud" class="facebook"
+                                          target="_blank"><i class="bx bxl-facebook"></i></a>
+                                      <a href="https://instagram.com/shri.krishna.jewellers" class="instagram"
+                                          target="_blank"><i class="bx bxl-instagram"></i></a>
+                                      <a href="https://g.page/r/CZ6RgBm0-OdQEAg/review" class="google-plus"
+                                          target="_blank"><i class="bx bxl-google"></i></a>
+                                  </div>
+                                  <br>
+                              </div>
+                          </div>
+                          <div class="col-md-6">
+                              <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
+                                  <i class="bx bx-envelope"></i>
+                                  <h3>Email</h3>
+                                  <p><a target="_blank"
+                                          href="mailto:support@shrikrishnajewellers.com">support@shrikrishnajewellers.com</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+
+                  </div>
+
+                  <div class="col-lg-6">
+
+                      <form action="forms/contact.php" method="post" role="form" class="php-email-form"
+                          data-aos="fade-up">
+                          <div class="section-title">
+                              <h2 data-aos="fade-in">Get In Touch With Us</h2>
+                          </div>
+                          <div class="form-row">
+                              <div class="col-md-6 form-group">
+                                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name"
+                                      data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                  <div class="validate"></div>
+                              </div>
+                              <div class="col-md-6 form-group">
+                                  <input type="email" class="form-control" name="email" id="email"
+                                      placeholder="Your Email" data-rule="email"
+                                      data-msg="Please enter a valid email" />
+                                  <div class="validate"></div>
+                              </div>
+                          </div>
+
+                          <div class="form-group">
+                              <input type="text" class="form-control" name="phone" id="phone" placeholder="Phone"
+                                  data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                              <div class="validate"></div>
+                          </div>
+
+                          <div class="form-group">
+                              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"
+                                  data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                              <div class="validate"></div>
+                          </div>
+                          <div class="form-group">
+                              <textarea class="form-control" name="message" rows="5" data-rule="required"
+                                  data-msg="Please write something for us" placeholder="Message"></textarea>
+                              <div class="validate"></div>
+                          </div>
+                          <div class="mb-3">
+                              <div class="loading">Loading</div>
+                              <div class="error-message"></div>
+                              <div class="sent-message">Your message has been sent. Thank you!</div>
+                          </div>
+                          <div class="text-center"><button type="submit">Send Message</button></div>
+                      </form>
+                  </div>
+
               </div>
 
-              <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validate"></div>
-              </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                <div class="validate"></div>
-              </div>
-              <div class="mb-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
           </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Contact Section -->
+      </section><!-- End Contact Section -->
 
   </main><!-- End #main -->
 
